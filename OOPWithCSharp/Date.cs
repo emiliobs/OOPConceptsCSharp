92e29d0bdfb243ee38033bf0884a7ef855ff78c8
emilio barrera <barrera_emilio@yahoo.es>
2023-02-10 16:31:02 +0000

    Attributtes, Properties , Methos and region in class Date

--- OOPWithCSharp/Date.cs
+++ OOPWithCSharp/Date.cs
@@ -2,17 +2,71 @@
 {
     public class Date
     {
-        public int Year { get; set; }
 
-        public int Month { get; set; }
+        #region Attributes
+        private int _year;
 
-        public int Day { get; set; }
+        private int _month;
+
+        private int _day;
+
+        #endregion
 
 
-        public override string ToString()
+        #region Contructors
+        public Date()
         {
-            return $"{Year, 4:0000}/{Month, 2:00}/{Day, 2:00}";
+            _year = 1900;
+            _month = 1;
+            _day = 1;
         }
+
+        public Date(int year, int month, int day)
+        {
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);
+            _day = ValidateDay(day);
+        }
+        #endregion
+
+
+        #region Properties
+        public int Year
+        {
+            get => _year;
+            set => _year = value;
+        }
+
+        public int Month
+        {
+            get => _month;
+            set => _month = value;
+        }
+
+        public int Day
+        {
+            get => _day;
+            set => _day = value;
+        }
+        #endregion
+
+        #region Methods
+        private int ValidateYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+        private int ValidateMonth(int month)
+        {
+            throw new NotImplementedException();
+        }
+        private int ValidateDay(int day)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override string ToString() => $"{Year,4:0000}/{Month,2:00}/{Day,2:00}"; 
+        #endregion
 
     }
 }
